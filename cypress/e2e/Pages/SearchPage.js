30e2d25b8cccddb8f01b7ed7c0a018414af96ccb
Mahmoud Raafat <91028184+MahmoudR2ft@users.noreply.github.com>
2022-07-29 03:01:04 +0200

    update

--- cypress/e2e/Pages/SearchPage.js
+++ cypress/e2e/Pages/SearchPage.js
@@ -1,7 +1,7 @@
 export class SearchPage{
     elements = {
         SamsungBrand : ()=> cy.xpath('//body[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[3]/span[1]/div[1]/div[1]/div[1]/div[4]/ul[1]/li[1]/span[1]/a[1]/div[1]/label[1]/i[1]'),
-        ProductCard : ()=> cy.xpath("//body/div[@id='a-page']/div[@id='search']/div[1]/div[1]/div[1]/span[3]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/span[1]/a[1]/div[1]")
+        ProductCard : ()=> cy.xpath("[data-asin="B09N41RC6L"] > .sg-col-inner > .s-widget-container > .s-card-container > .a-spacing-base > .a-spacing-small > .s-title-instructions-style > .a-size-mini > .a-link-normal > .a-size-base-plus")
     }
 
     CheckBrand(){
